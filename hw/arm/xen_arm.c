94fad9011a11e53ef3ed64e0fe95154c9a670913
Mykyta Poturai <Mykyta_Poturai@epam.com>
2023-12-19 14:09:52 +0000

    xen_arm: its: Introduce arm_gicv3_its_xen
    Introduce the Xen variant of Arm ITS. It is hooked into the mmio
    subsystem and recieves MSI messages. Upon receiving the MSI messages it
    gets transfered to the Xen hypervisor with the new device model call for
    further processing.
    
    This change will allow for virtio-pci backends to send MSI interrupts
    to the Xen guests.
    
    Change marked as RFC to get some feedback on the approach while the
    relevant Xen changes are being reviewed.
    
    Link to relevant Xen changes:
    https://patchwork.kernel.org/project/xen-devel/patch/6a631756a126e73390f95b9e86c69e3286c92f59.1702991909.git.mykyta_poturai@epam.com
    
    Signed-off-by: Mykyta Poturai <mykyta_poturai@epam.com>

--- hw/arm/xen_arm.c
+++ hw/arm/xen_arm.c
@@ -374,6 +374,7 @@ static void xen_enable_tpm(void)
 static void xen_arm_init(MachineState *machine)
 {
     XenArmState *xam = XEN_ARM(machine);
+    DeviceState *dev;
     int rc;
 
     if (!xen_enabled()) {
@@ -405,6 +406,11 @@ static void xen_arm_init(MachineState *machine)
 
     xen_enable_tpm();
 
+    dev = qdev_new("arm-its-xen");
+
+    sysbus_realize_and_unref(SYS_BUS_DEVICE(dev), &error_fatal);
+    sysbus_mmio_map(SYS_BUS_DEVICE(dev), 0, GUEST_GICV3_ITS_BASE);
+    msi_nonbroken = true;
     return;
 }
 
